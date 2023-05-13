882bfb5f5a03a5b613c197ae4de6a1824942fd20
PeryCreep <polyakov0202@bk.ru>
2023-05-13 19:39:50 +0500

    add new response to login user

--- src/main/scala/model/User.scala
+++ src/main/scala/model/User.scala
@@ -1,11 +1,19 @@
 package model
 import slick.jdbc.PostgresProfile.api._
-import spray.json.DefaultJsonProtocol.{BooleanJsonFormat, LongJsonFormat, StringJsonFormat, jsonFormat5}
+import spray.json.DefaultJsonProtocol.{BooleanJsonFormat, LongJsonFormat, StringJsonFormat, jsonFormat3, jsonFormat5}
 import spray.json.RootJsonFormat
 case class User(id: Long, name: String, email: String, hashedPassword: String, isOrganization: Boolean)
 
+case class UserProfileInfo(name: String, email: String, isOrganization: Boolean)
+object UserProfileInfo {
+  def fromUser(user: User) = {
+    UserProfileInfo(user.name, user.email, user.isOrganization)
+  }
+}
+
 object UserJson {
   implicit val jsonFormat: RootJsonFormat[User] = jsonFormat5(User.apply)
+  implicit val userInfoJsonFormat: RootJsonFormat[UserProfileInfo] = jsonFormat3(UserProfileInfo.apply)
 }
 
 class Users(tag: Tag) extends Table[User](tag, "users") {
