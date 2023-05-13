f18d7f3ee651672b50065ecc1a1865f4e14626d1
PeryCreep <polyakov0202@bk.ru>
2023-05-13 13:07:48 +0500

    rewrite some stub methods

--- src/main/scala/repositories/organization/OrganizationRepository.scala
+++ src/main/scala/repositories/organization/OrganizationRepository.scala
@@ -3,8 +3,8 @@ package repositories.organization
 import model.organization.Organization
 
 trait OrganizationRepository[F[_]] {
-  def getAllInRange(range: Double): F[Seq[Organization]]
+  def getAllInRange(range: Double, latitude: Double, longitude: Double): F[Seq[Organization]]
   def getAll: F[Seq[Organization]]
-  def getByOwnerId(id: Long):F[Organization]
-  def getByName(name: String): F[Organization]
+  def getByOwnerId(id: Long):F[Option[Organization]]
+  def getByName(name: String): F[Option[Organization]]
 }
