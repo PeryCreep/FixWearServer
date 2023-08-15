856d289d59c2818145313ccf861aabd772f0d7fa
PeryCreep <polyakov0202@bk.ru>
2023-08-15 21:53:06 +0500

    add open api spec and add new module to project

--- src/main/scala/routes/auth/AuthRoutes.scala
+++ src/main/scala/routes/auth/AuthRoutes.scala
@@ -1,6 +1,7 @@
 package routes.auth
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import model.User
@@ -23,12 +24,12 @@ object AuthRoutes {
           val result = userService.createUser(user)
           onComplete(result) {
             case Success(_) => complete("Регистрация прошла успешно")
-            case Failure(ex) => complete(ex.getMessage)
+            case Failure(ex) => complete(StatusCodes.BadRequest, ex.getMessage)
           }
         }
       }
     } ~
-      path("login") {
+      path("login") {//todo не надо кадать всего user-a нужно только логин и пароль
         post {
           entity(as[User]) { user => //todo наверное надо сделать DTO
             onSuccess(userService.authenticateUser(user.email, user.hashedPassword)) { result =>
